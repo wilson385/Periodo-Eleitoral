bcaaa728928c8ee35d4447f05c46bf779a7203f7
Wilson Cristian <wilson.cristvao@gmail.com>
2025-09-23 21:09:19 -0300

    adicionando script para limpar o campo do input

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function enviar() {
-
-const nascimento = document.querySelector('input').value.trim()
+const nascimento_Input = document.querySelector("input");
+const nascimento_Valor = document.querySelector('input').value.trim();
 // ano atual
 const ano_Atual = new Date().getFullYear();
-const idade = ano_Atual - nascimento;
+const idade = ano_Atual - nascimento_Valor;
 const paragrafo = document.getElementById('paragrafo');
 
 // enquanto for vazio, 
 // espaços ou não numérico, continua pedindo
-if (nascimento === "" || isNaN(nascimento)) {
+if (nascimento_Valor === "" || isNaN(nascimento_Valor)) {
     alert("Por favor, insira um ano de nascimento válido.");
     return; // Importante: impede que o resto da
     //  função execute se o valor for vazio ou inválido
@@ -30,6 +30,9 @@ else if (idade >= 18 && idade <65) {
 else if (idade >= 65) {
     paragrafo.textContent = `Com ${idade} anos, o voto é opcional.`; 
 }
+
+// limpa o campo após o envio
+nascimento_Input.value = "";
 
 
 }
